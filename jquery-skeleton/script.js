2b59cc48cfea595f07ab5b4181b7f0fb6b27faa4
kevinl94303 <kevinl94303spam@gmail.com>
2018-10-04 20:46:53 -0400

    added jquery skeleton

---
+++ jquery-skeleton/script.js
@@ -0,0 +1,19 @@
+users = [
+"Billy Chiu",
+"Matt Vanegas",
+"Irene Nam",
+"Raymond Li",
+"Arsalaan Ansari"
+]
+
+comments = [
+"Guys the trainees are too smart",
+"How do we keep them busy?",
+"Let's make them do learnyounode for three meetings",
+"Yuh yeet",
+"You bum"
+]
+
+$(document).ready(function(){
+	alert("hello!") // replace this line
+})
